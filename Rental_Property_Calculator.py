9e12b22f86f5ef056659ab278c2a880c7c412a79
Jason Stumpf <jstumpf518@yahoo.com>
2023-06-05 10:31:46 -0700

    Made Improvements

--- Rental_Property_Calculator.py
+++ Rental_Property_Calculator.py
@@ -7,17 +7,27 @@ class Rental_property_calculator():
 
     def income(self):
         self.rental_income = input("\nWhat is your monthy rental income: ")
-        question = input("Do you have any additional related income (Laundry, Storage, etc.): ")
-        if question.lower() == 'yes' or question.lower() == 'y':
-            extra = input("How much is it: ")
-            self.rental_income = int(self.rental_income) + int(extra)
-            print(f"Your total income is ${self.rental_income}")
-        elif question.lower() == 'no' or question.lower() == 'n':
-            print(f"Your total income is ${self.rental_income}")
+        question1 = input("Do you have any additional related income (Laundry, Storage, etc.): ")
+        if question1.lower() == 'yes' or question1.lower() == 'y':
+            extra1 = input("How much is it: ")
+            self.rental_income = int(self.rental_income) + int(extra1)
+            print(f"Your total monthly income is ${self.rental_income}")
+        elif question1.lower() == 'no' or question1.lower() == 'n':
+            print(f"Your total monthly income is ${self.rental_income}")
 
     def expenses(self):
-        self.property_expenses = input("\nWhat is your total monthy expenses: ")
-        print(f"Your total expenses is ${self.property_expenses}")
+        diary_exp = {}
+        print("\nEnter your expenses, than enter how much they cost. Type DONE when finished.")
+        while True:
+            key = input("What expense do you have: ")
+            if key.lower() == 'done':
+                break
+            value = input("How much does that cost: ")
+            diary_exp[key] = value
+            
+        for x in diary_exp.values():
+            self.property_expenses += int(x)
+        print(f"\nYour total monthly expenses is ${self.property_expenses}")
 
     def cash_flow(self):
         self.current_cash_flow = int(self.rental_income) - int(self.property_expenses)
@@ -26,9 +36,22 @@ class Rental_property_calculator():
     def return_on_investment(self):
         self.current_cash_flow = int(self.rental_income) - int(self.property_expenses)
         annual = (self.current_cash_flow * 12)
-        total = input("\nWhat was the total amount of the investment: ")
+        print("\nBefore getting your ROI, you'll need to enter the amount of money that you put down for the property.")
+        while True:
+            d_p = input("\nHow much was the down payment: ")
+            c_c = input("How much was the closing cost: ")
+            question2 = input("Where there any additional money spect (Rehbas, etc.): ")
+            if question2.lower() == 'yes' or question2.lower() == 'y':
+                extra2 = input("How much was it: ")
+                total = (int(d_p) + int(c_c) + int(extra2))
+                print(f"Your total investment was {total}")
+                break
+            elif question2.lower() == 'no' or question2.lower() == 'n':
+                total = (int(d_p) + int(c_c))
+                print(f"Your total investment was {total}")
+                break  
         roi = ((int(annual) / int(total)) * 100)
-        print(f"Your cash on cash ROI is {roi}%")
+        print(f"\nYOUR CASH ON CASH RETURN ON INVESTMENT IS {roi}%")
 
 landlord = Rental_property_calculator()
 
